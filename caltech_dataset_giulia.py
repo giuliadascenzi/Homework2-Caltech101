6496e0680112f9dfb8912a0851c5227c83579d41
Giulia D’Ascenzi <73444004+giuliadascenzi@users.noreply.github.com>
2021-10-29 14:30:34 +0200

    Update caltech_dataset_giulia.py

--- caltech_dataset_giulia.py
+++ caltech_dataset_giulia.py
@@ -29,13 +29,15 @@ class Caltech(VisionDataset):
         
         
         self.labels = {} #save a dictionary of labels
-        self.targets = []
+        targets = []
         for line in self.data.values:
           folder = line[0].split("/")[0]
-          self.targets.append(folder)
+          targets.append(folder)
           if folder not in self.labels:
             self.labels[folder]= len(self.labels)
         
+        t = pd.Series(targets)
+        self.data[:,1] = t
         
 
         '''
